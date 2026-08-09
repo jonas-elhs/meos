8c29461006c8b94794dbe5cd446a3f5c6a106b93
jonas-elhs <jonas.elhs@outlook.com>
2026-08-09 17:42:05 +0200

    fix(fish): syntax highlights

--- hosts/mixos/fish.nix
+++ hosts/mixos/fish.nix
@@ -14,13 +14,15 @@
       ];
       configFile.content = lib.concatStringsSep "\n" [
         ''
+          # Config
           fish_vi_key_bindings
           set -g fish_autosuggestion_enabled 0
           set -g fish_greeting
 
+          # Syntax Highlighting
           set -g fish_color_autosuggestion brblack
           set -g fish_color_cancel -r
-          set -g fish_color_command normal
+          set -g fish_color_command --reset
           set -g fish_color_comment red
           set -g fish_color_cwd green
           set -g fish_color_cwd_root red
@@ -28,9 +30,9 @@
           set -g fish_color_error brred
           set -g fish_color_escape brcyan
           set -g fish_color_history_current --bold
-          set -g fish_color_host normal
+          set -g fish_color_host --reset
           set -g fish_color_host_remote yellow
-          set -g fish_color_normal normal
+          set -g fish_color_normal --reset
           set -g fish_color_operator brcyan
           set -g fish_color_param cyan
           set -g fish_color_quote yellow
@@ -40,10 +42,8 @@
           set -g fish_color_status red
           set -g fish_color_user brgreen
           set -g fish_color_valid_path --underline
-
-          set -g fish_pager_color_completion normal
           set -g fish_pager_color_description yellow -i
-          set -g fish_pager_color_prefix normal --bold --underline
+          set -g fish_pager_color_prefix --bold --underline
           set -g fish_pager_color_progress brwhite --background=cyan
           set -g fish_pager_color_selected_background -r
         ''
