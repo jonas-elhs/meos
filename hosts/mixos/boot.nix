0d6b2c927e19a5e5471280a4d4a56b195c2b2f8d
jonas-elhs <jonas.elhs@outlook.com>
2026-08-21 14:15:02 +0200

    feat(plymouth): add

--- hosts/mixos/boot.nix
+++ hosts/mixos/boot.nix
@@ -1,13 +1,21 @@
-{pkgs, ...}: {
-  boot.loader = {
-    timeout = 0;
-    efi.canTouchEfiVariables = true;
+{
+  boot = {
+    plymouth.enable = true;
+    initrd.verbose = false;
+    consoleLogLevel = 3;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+    ];
 
-    systemd-boot = {
-      enable = true;
-      editor = false;
+    loader = {
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+
+      systemd-boot = {
+        enable = true;
+        editor = false;
+      };
     };
   };
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
