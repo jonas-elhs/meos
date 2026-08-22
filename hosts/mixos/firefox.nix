e4b6e3f88f22b9f438319b20a05b1de6c1ce3e89
jonas-elhs <jonas.elhs@outlook.com>
2026-08-22 14:16:18 +0200

    feat(firefox): add unhook and privacy badger extensions

--- hosts/mixos/firefox.nix
+++ hosts/mixos/firefox.nix
@@ -8,13 +8,23 @@
       ExtensionSettings = {
         # uBlock Origin
         "uBlock0@raymondhill.net" = {
-          installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
         };
         # 1Password
         "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-          installation_mode = "force_installed";
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4951729/1password_x_password_manager-8.12.32.33.xpi";
+          installation_mode = "force_installed";
+        };
+        # Privacy Badger:
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # Unhook
+        "myallychou@gmail.com" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4733035/youtube_recommended_videos-1.6.9.xpi";
         };
       };
     };
