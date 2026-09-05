c65a75022b897d947a1f6478eae1406a2343cecc
jonas-elhs <jonas.elhs@outlook.com>
2026-09-05 19:44:06 +0200

    feat(fonts): use maple-mono variable branch

--- hosts/mixos/fonts.nix
+++ hosts/mixos/fonts.nix
@@ -2,7 +2,32 @@
   fonts.fontconfig.enable = true;
 
   fonts.packages = with pkgs; [
-    pkgs.maple-mono.NF-unhinted
+    (pkgs.stdenv.mkDerivation {
+      pname = "MapleMono-NF-unhinted";
+      version = "8.0b2";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "subframe7536";
+        repo = "maple-font";
+        rev = "variable";
+        hash = "sha256-PnHTFH8qU9aZHvfuRnI7/jkCyTgSNykCqSB2jqIws7M=";
+      };
+
+      nativeBuildInputs = with pkgs.python3Packages; [
+        py7zr
+        fontmake
+        ttfautohint-py
+      ];
+
+      buildPhase = ''
+        python3 build.py --no-hinted
+      '';
+      installPhase = ''
+        find . -name '*.ttf'    -exec install -Dt $out/share/fonts/truetype {} \;
+        find . -name '*.otf'    -exec install -Dt $out/share/fonts/opentype {} \;
+        find . -name '*.woff2'  -exec install -Dt $out/share/fonts/woff2 {} \;
+      '';
+    })
 
     material-symbols
   ];
